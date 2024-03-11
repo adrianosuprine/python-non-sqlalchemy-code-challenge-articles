fa9c8270debaf511b29c995ebdb3a9499eb744d2
adrianosuprine <adrianosuprine@gmail.com>
2024-03-11 05:11:54 +0300

    article class completed

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,8 +1,21 @@
 class Article:
+    # class attribute that stores all magazines
+    all = []
+    
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
-        self.title = title
+        self._title = str(title)
+        # appends data to the empty list
+        Article.all.append(self)
+        
+    @property
+    def title(self):
+        return self._title
+    @title.setter
+    def title(self,new_title):
+        return new_title
+        
         
 class Author:
     def __init__(self, name):
@@ -36,3 +49,5 @@ class Magazine:
 
     def contributing_authors(self):
         pass
+
+# 
